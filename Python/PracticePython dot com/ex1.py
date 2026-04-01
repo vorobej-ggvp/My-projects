c4db4e10566a93d0ab9d6e28f5bea0f360cd232b
Vorobej94 <dvorobej94@gmail.com>
2026-04-01 15:02:25 +0300

    getting rid of odd files

--- Python/PracticePython dot com/ex1.py
+++ Python/PracticePython dot com/ex1.py
@@ -1,11 +1,11 @@
-from datetime import date
-hundred = 100
+def fib(n):
+    pass
+    
 
-name = input("Write your name: ")
-age  = int(input("Write your age: "))
-amount_of_times = int(input("How many times to output: "))
-time_left = hundred - age
-today = date.today()
-curr_year = int(today.strftime("%Y"))
-year_hundred = curr_year + time_left
-print(amount_of_times * (name + ", you will get 100 years old in " + str(time_left) + ", so it will be " + str(year_hundred) + "\n"))
+def main():
+    n = int(input())
+    print(fib(n))
+
+
+if __name__ == "__main__":
+    main()
--- Python/PracticePython dot com/ex2.py
+++
@@ -1,2 +0,0 @@
-number = input("Enter a number: ")
-number % 2 ? print("even") : print("odd")
