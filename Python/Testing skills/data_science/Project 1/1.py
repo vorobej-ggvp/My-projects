dbad9828ae24c276179a091f9227e404080ba0ef
Vorobej94 <dvorobej94@gmail.com>
2026-04-01 17:16:46 +0300

    Finished first good project with pandas (finally)

--- Python/Testing skills/data_science/Project 1/1.py
+++ Python/Testing skills/data_science/Project 1/1.py
@@ -1,19 +1,69 @@
+import mlcroissant as mlc
+import plotly.graph_objects as go
 import pandas as pd
-import numpy as np
+import logging
+import warnings
 
-# 1. Создаем тестовые данные (имитация потребления энергии)
-date_rng = pd.date_range(start='2025-01-01', end='2025-01-10', freq='h')
-df = pd.DataFrame(date_rng, columns=['date'])
-df['usage'] = np.random.randint(50, 100, size=(len(date_rng)))
+logging.getLogger('absl').setLevel(logging.ERROR)
 
-# 2. Алгоритм создания лагов (важно для предсказания)
-def create_lags(data, n_lags):
-    for i in range(1, n_lags + 1):
-        data[f'lag_{i}'] = data['usage'].shift(i)
-    return data
+warnings.filterwarnings("ignore")
 
-# 3. Применяем и удаляем пустые строки, возникшие из-за сдвига
-df = create_lags(df, 3).dropna()
+# Fetch the Croissant JSON-LD
+croissant_dataset = mlc.Dataset('https://www.kaggle.com/datasets/awadhi123/finance-data-sp-500/croissant/download')
 
-print("Данные с признаками для предсказания:")
-print(df.head())
+# Check what record sets are in the dataset
+record_sets = croissant_dataset.metadata.record_sets
+
+# Fetch the records and put them in a DataFrame
+record_set_df = pd.DataFrame(croissant_dataset.records(record_set=record_sets[0].uuid))
+
+# Split name values for readability; Default: SP500.csv/{col_name}
+record_set_df.columns = [col.split('/')[-1].replace('+', '_').lower() for col in record_set_df.columns]
+
+# ---------------------------------------------------------
+
+# Column 'Date' to date format
+record_set_df['date'] = pd.to_datetime(record_set_df['date'])
+
+# Make 'Date' column as index
+record_set_df.set_index('date', inplace=True)
+clean_df = record_set_df.interpolate(method='time')
+
+# Because 'close' and 'adj_close' are identical
+clean_df.drop(columns=['close'], inplace=True)
+
+# ---------------------------------------------------------
+
+# Make daily return
+clean_df['daily_return'] = clean_df['adj_close'].pct_change()
+
+# Make volatility for 20 days
+clean_df['volatility_20d'] = clean_df['daily_return'].rolling(window=20).std()
+
+# ---------------------------------------------------------
+
+# Make lag_1 as price_today - price_yesterday
+clean_df['lag_1'] = clean_df['adj_close'].shift(1)
+# Make mean for week
+clean_df['rolling_mean_5'] = clean_df['adj_close'].rolling(window=5).mean()
+
+# Delete unescessary rows (noise)
+clean_df.dropna(inplace=True)
+
+# ---------------------------------------------------------
+
+# Make figure
+fig = go.Figure()
+
+fig.add_trace(go.Scatter(x=clean_df.index, y=clean_df['adj_close'], name="Closing price", line=dict(color="blue")))
+
+fig.add_trace(go.Scatter(x=clean_df.index, y=clean_df['rolling_mean_5'], name="Trand (5 days)", line=dict(color="orange", dash="dash")))
+
+fig.update_layout(
+    title='Analysis of index S&P 500',
+    xaxis_title='Date',
+    yaxis_title='Price($)',
+    hovermode='x unified',
+)
+
+fig.show()
