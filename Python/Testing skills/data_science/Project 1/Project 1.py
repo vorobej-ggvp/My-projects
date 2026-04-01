41cb94b7c81e4bd61b6b3b7c4ac8bde5d4b6b9d2
Vorobej94 <dvorobej94@gmail.com>
2026-04-01 18:20:21 +0300

    Added AI for predicting prices to 'Project 1'.py

--- Python/Testing skills/data_science/Project 1/Project 1.py
+++ Python/Testing skills/data_science/Project 1/Project 1.py
@@ -1,3 +1,5 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
 import mlcroissant as mlc
 import plotly.graph_objects as go
 import pandas as pd
@@ -34,18 +36,16 @@ clean_df.drop(columns=['close'], inplace=True)
 
 # ---------------------------------------------------------
 
+# Make lag_1 as price_today - price_yesterday
+clean_df['lag_1'] = clean_df['adj_close'].shift(1)
+# Make mean for week
+clean_df['rolling_mean_5'] = clean_df['adj_close'].rolling(window=5).mean()
+
 # Make daily return
 clean_df['daily_return'] = clean_df['adj_close'].pct_change()
 
 # Make volatility for 20 days
 clean_df['volatility_20d'] = clean_df['daily_return'].rolling(window=20).std()
-
-# ---------------------------------------------------------
-
-# Make lag_1 as price_today - price_yesterday
-clean_df['lag_1'] = clean_df['adj_close'].shift(1)
-# Make mean for week
-clean_df['rolling_mean_5'] = clean_df['adj_close'].rolling(window=5).mean()
 
 # Delete unescessary rows (noise)
 clean_df.dropna(inplace=True)
@@ -67,3 +67,49 @@ fig.update_layout(
 )
 
 fig.show()
+
+# ---------------------------------------------------------
+
+split_index = len(clean_df) - 252           # 252 – ~ one business year
+train_df = clean_df.iloc[:split_index]      # Train data
+test_df = clean_df.iloc[split_index:]       # Test data
+
+
+X_train = train_df[['lag_1', 'rolling_mean_5', 'volatility_20d']]
+y_train = train_df['adj_close']
+
+X_test = test_df[['lag_1', 'rolling_mean_5', 'volatility_20d']]
+y_test = test_df['adj_close']
+
+
+# Create a model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+error = mean_absolute_error(y_test, predictions)    # Result: 31.80264219020117 $
+
+# --------------------------------------------------------
+
+# Make another figure
+fig1 = go.Figure()
+
+fig1.add_trace(go.Scatter(x=X_test.index, y=y_test, name="Real prices", line=dict(color="blue")))
+fig1.add_trace(go.Scatter(x=X_test.index, y=predictions, name="Predicted prices", line=dict(color="orange", dash='dash')))
+fig1.update_layout(
+    title='Training model to prognoze tomorrow price',
+    xaxis_title='Real values',
+    yaxis_title='Predicted values',
+    hovermode='x unified',
+)
+
+fig1.show()
+
+# --------------------------------------------------------
+
+X_for_tomorrow = clean_df[['lag_1', 'rolling_mean_5', 'volatility_20d']].tail(1) 
+
+print(model.predict(X_for_tomorrow))        # Result: 3225.5669444$
+
+# Conclusion: Line with predicted values goes a bit after line with real values. 
